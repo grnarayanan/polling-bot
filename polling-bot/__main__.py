b911ec77e6d4c52d2c3f56b6ce143c83183dce00
Ganesan Narayanan <grn2112@columbia.edu>
2023-03-03 18:38:15 -0500

    added testing, linting, static analysis

---
+++ polling-bot/__main__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+polling-bot for Discord
+
+Conduct basic polling functions in a Discord server
+
+@author: Ganesan Narayanan
+"""
+
+import os
+import discord
+from dotenv import load_dotenv
+from polling_bot import PollingBot
+
+load_dotenv()
+token = os.getenv('TOKEN')
+
+bot = PollingBot(intents=discord.Intents.all(), command_prefix="/")
+
+bot.run(token)
